6b7966c7c126a35c1bd702792ada6ac0b9773172
Eun-Hee <hd96010@daum.net>
2021-02-28 14:19:35 +0900

    가격ㅍ시  - 억 넘는지 표시

--- app/src/main/java/com/tjoeun/jickbangcopy_20210228/adapters/RoomAdapter.kt
+++ app/src/main/java/com/tjoeun/jickbangcopy_20210228/adapters/RoomAdapter.kt
@@ -41,8 +41,14 @@ class RoomAdapter(
 //        주소/층수 => ~~시 ~~구, 5층 => String 가공
 
 //        층수 : 1이상, 0, -1 이하냐에 따라 다르게 가공되어야 함
-//
+//        상황에 따라 다르게 층수를 가공하는 기능을 => Room의 기능으로 추가해보자
+
         addressAndFloorTxt.text = "${data.address}, ${data.getFormattedFloor()}"
+
+//        가격 표시 : price가 1만 이상, 아니냐에 따라 다르게 가공
+//        1만 미만 : 8500등 컴마만. 1만 이상 : 5억 4,500 등으로 억단위를 별도 표기
+//        해당 기능도 Room의 기능으로 추가해두고 => 가져다쓰자.
+        priceTxt.text =
         return row
     }
 
--- app/src/main/java/com/tjoeun/jickbangcopy_20210228/datas/Room.kt
+++ app/src/main/java/com/tjoeun/jickbangcopy_20210228/datas/Room.kt
@@ -1,12 +1,41 @@
 package com.tjoeun.jickbangcopy_20210228.datas
 
+import android.icu.number.NumberRangeFormatterSettings
+import java.text.NumberFormat
+import java.util.*
+
 class Room(
     val price: Int,
     val address: String,
     val floor: Int,
     val description: String) {
+
+//    가격 값에 따라 => 억단위 표시 여부 가공 기능 => 함수로
+
+    fun getFormattedPrice() : String {
+
+    if (this.price < 100000) {
+//        8,500 등 컴마처리
+
+        return NumberFormat.getNumberInstance(Locale.US).format(this.price)
+    }
+    else{
+//        1억 이상, ?억4,500등 억단위 + 컴마처리 둘다
+
+//        억? 몇억인지 => 가격을 10000으로 나문 몫임
+        val uk = this.price / 10000
+
+//        억 밑은 얼마인지:\?
+        val rest = this.price%10000
+//        rest => 컴마 처리
+        val commaRest = NumberFormat.getNumberInstance(Locale.US).format(this.price)
+
+    }
+    }
+
+
 //    층수 값에 따라 => 5층, 반지하, 지하 2층 등으로 가공해주는 기능 => 함수 (function)
-    
+
     fun getFormattedFloor() : String {
         
 //        층수 값에 따라 다른 값을 return
@@ -19,7 +48,8 @@ class Room(
             return "반지하"
         }
         else {
-//            -1층, -2층
+//            -1층, -2층.. => 지하 1층, 지하 2층 등으로 가공.
+            return "지하 ${-this.floor}층"
         }
     }
 
